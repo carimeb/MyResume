3dddf99cf9065773c085d132b5639b12edb3501d
carimeb <44734986+carimeb@users.noreply.github.com>
2019-08-02 16:07:27 -0300

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 public class {
-
-  public static void main(String args[]) {
-    System.out.println("Hello World!");
-    
-  }
   
+  public static void main(String args[]) {
+    System.out.println("Hello World!");  
+  
+  }
+
 }
